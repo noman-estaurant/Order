c7a95fcc0a4cf8573cdbaf12ed6e6e0f47f93bca
emschenn <emilynov14@gmail.com>
2019-05-09 22:26:48 +0800

    96%

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
 $(document).ready(function() {
-
+    $(".navigationbar .cart .added").hide();
+    // if (added != 0) {
+    //     $(".navigationbar .cart .added").show();
+    // }
 });
 
 
 
 
 $("#big-menu #main-meal").click(function() {
-    console.log("hello");
-    $("#big-menu #main-meal").animate({
+    $(this).animate({
         backgroundColor: "rgb(255,123,159)",
         boxShadow: "0px 10px 20px rgba(158, 77, 100, 0.5)",
     }, 150);
-    $("#big-menu").hide(600, function() {
-        //change to main-meal menu
-    });
+    //change to main-meal menu
 });
 $("#big-menu #side-meal").click(function() {
-    $("#big-menu #side-meal").animate({
+    $(this).animate({
         backgroundColor: "rgb(255,123,159)",
         boxShadow: "0px 10px 20px rgba(158, 77, 100, 0.5)",
     }, 150);
-    $("#big-menu").hide(600, function() {
-        //change to side-meal menu
-    });
+    //change to side-meal menu
+
 });
 $("#big-menu #drinks").click(function() {
-    $("#big-menu #drinks").animate({
+    $(this).animate({
         backgroundColor: "rgb(255,123,159)",
         boxShadow: "0px 10px 20px rgba(158, 77, 100, 0.5)",
     }, 150);
-    $("#big-menu").hide(600, function() {
-        //change to drinks menu
-    });
+    //change to drinks menu
+
 });
